b27aa44269caf5ac88c90612b1e4fe212c408906
Gurkanwal Singh Kang <gurkanwal@Gurkanwals-MacBook-Air.local>
2022-06-09 00:28:37 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ZotVerse
+Quick and easy auctions.
+
+## Features that work so far
++ Register an account
++ Login / Logout
++ Create auctions
++ Bid on auctions
++ Update bid
++ View all auctions (newest first)
++ View active auctions (oldest first)
++ View "My auctions"
++ View "My bids"
++ Auction resolution
++ Search for Products
---
+++ auctionrush/views.py
@@ -0,0 +1,35 @@
+#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("/")
+            # return HttpResponseRedirect(reverse('auctions'))
+        else:
+            for msg in form.error_messages:
+                # print(form.error_messages[msg])
+                #print(msg)
+                error_message = form.error_messages[msg]
+
+            return render(request = request,
+                          template_name = "registration/register.html",
+                          context={
+                              "form": form,
+                              "error_message": error_message,
+                          })
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "registration/register.html",
+                  context={"form":form})
+
+def success(request):
+    return render(request , 'successPage.html')
